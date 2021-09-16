f8b8e075400babf0905cdc20178cb8bed74a0a99
sm42499 <89489372+sm42499@users.noreply.github.com>
2021-09-16 15:41:57 -0700

    Activity 2A completed

--- Level1.java
+++ Level1.java
@@ -16,5 +16,18 @@ public class Level1 extends World
     {    
         // Create a new world with 1200x800 cells with a cell size of 1x1 pixels.
         super(1200, 800, 1, false); 
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        setPaintOrder(Player.class, Platform.class, Obstacle.class, Collectable.class, Door.class, HUD.class);
+        Player player = new Player();
+        addObject(player,35,644);
+        Door door = new Door();
+        addObject(door,604,42);
     }
 }
